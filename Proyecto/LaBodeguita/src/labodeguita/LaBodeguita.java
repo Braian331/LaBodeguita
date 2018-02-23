b4dccac012b0655aec53df9e569926c90bb9e3b5
Braian Canjay <36526605+Braian331@users.noreply.github.com>
2018-02-22 20:28:07 -0700

    Add files via upload
    Inicio del Proyectos

---
+++ Proyecto/LaBodeguita/src/labodeguita/LaBodeguita.java
@@ -0,0 +1,97 @@
+<<<<<<< HEAD
+package labodeguita;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LaBodeguita {
+    //crear un objeto privado estatico en clase
+    // connection with parametres
+    private static Connection conn;
+    //crear variables privadas estaticas tipo string
+    private static final String driver="com.mysql.jdbc.Driver";
+    //referencia de archivo agregado
+    private static final String user="root";
+    private static final String password="";
+    private static final String url="jdbc:mysql://localhost:3306/jugueteria";
+    
+    //creando constructor de clase
+    public LaBodeguita() {
+    conn=null;
+        try {
+          Class.forName(driver);
+          //codigo de acceso para la base de datos
+          conn =DriverManager.getConnection(url,user, password);
+          //2.validacion de BD
+          
+          if(conn!=null){
+              System.out.println("Conexion establecida!!!");
+            }
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("Error de conexion :( "+ e);
+            }
+    }
+    
+    //metodo conectar
+    public Connection getConnection(){
+       
+        return conn; 
+    }
+    public void Desconectar(){
+        conn=null;
+        //validacion
+        if(conn==null){
+            System.out.println("Conexion terminada...");
+        }
+    }
+}
+=======
+package labodeguita;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LaBodeguita {
+    //crear un objeto privado estatico en clase
+    // connection with parametres
+    private static Connection conn;
+    //crear variables privadas estaticas tipo string
+    private static final String driver="com.mysql.jdbc.Driver";
+    //referencia de archivo agregado
+    private static final String user="root";
+    private static final String password="";
+    private static final String url="jdbc:mysql://localhost:3306/jugueteria";
+    
+    //creando constructor de clase
+    public LaBodeguita() {
+    conn=null;
+        try {
+          Class.forName(driver);
+          //codigo de acceso para la base de datos
+          conn =DriverManager.getConnection(url,user, password);
+          //2.validacion de BD
+          
+          if(conn!=null){
+              System.out.println("Conexion establecida!!!");
+            }
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("Error de conexion :( "+ e);
+            }
+    }
+    
+    //metodo conectar
+    public Connection getConnection(){
+       
+        return conn; 
+    }
+    public void Desconectar(){
+        conn=null;
+        //validacion
+        if(conn==null){
+            System.out.println("Conexion terminada...");
+        }
+    }
+}
+>>>>>>> b7cf10aa873a27e1e92ca103e85c5222f683c28d
