a3021e1b1cddbcff0de9202d46e39bde92fdbbfd
AlejandroPadilla0808 <36526647+AlejandroPadilla0808@users.noreply.github.com>
2018-03-17 16:23:33 -0600

    cambio de colores

--- src/labodeguita/ReabastecimientoDeStock.java
+++ src/labodeguita/ReabastecimientoDeStock.java
@@ -48,20 +48,22 @@ public class ReabastecimientoDeStock extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
         actual1 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        nombproducto = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        jPanel1.setBackground(new java.awt.Color(0, 153, 153));
+        jPanel1.setBackground(new java.awt.Color(0, 204, 204));
         jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jPanel1.setLayout(null);
 
-        jPanel2.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel2.setBackground(new java.awt.Color(102, 153, 255));
         jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jPanel2.setLayout(null);
 
+        jLabel1.setBackground(new java.awt.Color(0, 0, 0));
         jLabel1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Reabastecimiento de stock");
         jPanel2.add(jLabel1);
         jLabel1.setBounds(110, 10, 300, 40);
@@ -81,7 +83,7 @@ public class ReabastecimientoDeStock extends javax.swing.JFrame {
         listbusqueda.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "eliga tipo de busqueda...", "Nombre", "Codigo", "Descripcion", "Fecha", "Proveedor" }));
         listbusqueda.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jPanel2.add(listbusqueda);
-        listbusqueda.setBounds(466, 10, 160, 30);
+        listbusqueda.setBounds(470, 10, 160, 30);
 
         jPanel1.add(jPanel2);
         jPanel2.setBounds(0, 10, 880, 60);
@@ -101,55 +103,66 @@ public class ReabastecimientoDeStock extends javax.swing.JFrame {
         jScrollPane1.setViewportView(tbdatos);
 
         jPanel1.add(jScrollPane1);
-        jScrollPane1.setBounds(222, 90, 640, 220);
+        jScrollPane1.setBounds(220, 80, 640, 220);
 
         jLabel3.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("stock a reabastecer ");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(10, 150, 210, 17);
+        jLabel3.setBounds(10, 200, 210, 17);
 
         agregar.setText(" ");
         agregar.setToolTipText("cantidad que desea agregar a su stock");
         jPanel1.add(agregar);
-        agregar.setBounds(10, 170, 130, 30);
+        agregar.setBounds(10, 220, 130, 30);
 
         jLabel4.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Stock total");
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(10, 210, 90, 17);
+        jLabel4.setBounds(10, 260, 90, 17);
 
         total.setText(" ");
         total.setToolTipText("stock actual y el solicitado");
         jPanel1.add(total);
-        total.setBounds(10, 230, 130, 30);
+        total.setBounds(10, 280, 130, 30);
 
         jButton1.setText("Guardar");
         jButton1.setToolTipText("Aceptar reabastecimiento");
         jButton1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jPanel1.add(jButton1);
-        jButton1.setBounds(20, 290, 80, 30);
+        jButton1.setBounds(20, 340, 80, 30);
 
         jButton2.setText("Cancelar");
         jButton2.setToolTipText("Cancelar reabastecimiento");
         jButton2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jPanel1.add(jButton2);
-        jButton2.setBounds(110, 290, 90, 30);
+        jButton2.setBounds(110, 340, 90, 30);
 
         jLabel5.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("Stock disponible");
         jPanel1.add(jLabel5);
-        jLabel5.setBounds(10, 90, 110, 17);
+        jLabel5.setBounds(10, 140, 110, 17);
 
         actual1.setText(" ");
         actual1.setToolTipText("Stock actual");
         jPanel1.add(actual1);
-        actual1.setBounds(10, 110, 130, 30);
+        actual1.setBounds(10, 160, 130, 30);
+
+        jLabel6.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Nombre del producto");
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(10, 80, 140, 17);
+
+        nombproducto.setText(" ");
+        nombproducto.setToolTipText("Stock actual");
+        jPanel1.add(nombproducto);
+        nombproducto.setBounds(10, 100, 130, 30);
 
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 880, 330);
+        jPanel1.setBounds(0, 0, 880, 440);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -200,10 +213,12 @@ public class ReabastecimientoDeStock extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JComboBox<String> listbusqueda;
+    private javax.swing.JTextField nombproducto;
     private javax.swing.JTable tbdatos;
     private javax.swing.JTextField total;
     // End of variables declaration//GEN-END:variables
