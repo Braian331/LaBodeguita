3fa474b628cedc98a050ddf7084a4c4d5f6bc5d2
AlejandroPadilla0808 <36526647+AlejandroPadilla0808@users.noreply.github.com>
2018-04-06 16:23:17 -0600

    generacion de reportes de ventas.

--- src/labodeguita/Principal.java
+++ src/labodeguita/Principal.java
@@ -1,11 +1,21 @@
 package labodeguita;
 
+
+import java.io.FileOutputStream;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
 public class Principal extends javax.swing.JFrame {
@@ -53,6 +63,9 @@ public class Principal extends javax.swing.JFrame {
         valor = new javax.swing.JTextField();
         fecha = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu5 = new javax.swing.JMenu();
@@ -81,35 +94,45 @@ public class Principal extends javax.swing.JFrame {
         hora.setForeground(new java.awt.Color(255, 255, 255));
         hora.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         hora.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(hora, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 80, 220, 60));
+        getContentPane().add(hora, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 220, 60));
+
+        valor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                valorActionPerformed(evt);
+            }
+        });
         getContentPane().add(valor, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 20, 80, -1));
 
         fecha.setFont(new java.awt.Font("Century Gothic", 0, 36)); // NOI18N
         fecha.setForeground(new java.awt.Color(255, 255, 255));
         fecha.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         fecha.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(fecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 80, 360, 60));
+        getContentPane().add(fecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 20, 360, 60));
 
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
-        jLabel1.setIcon(new javax.swing.JLabel() {
-            public javax.swing.Icon getIcon() {
-                try {
-                    return new javax.swing.ImageIcon(
-                        new java.net.URL("file:/C:/Users/Braian Canjay/Desktop/Inte/fondo LB.png")
-                    );
-                } catch (java.net.MalformedURLException e) {
-                }
-                return null;
-            }
-        }.getIcon());
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/labodeguita/logotipo.png"))); // NOI18N
         jLabel1.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 750, 410));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 100, 240, 190));
 
-        jMenuBar1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "La Bodeguita", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Century Gothic", 0, 14), new java.awt.Color(255, 51, 51))); // NOI18N
+        jPanel1.setBackground(new java.awt.Color(255, 0, 0));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 700, 280));
 
-        jMenu1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 5));
-        jMenu1.setText("Inicio");
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/labodeguita/diseño pequeño.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 290, -1, -1));
+
+        jPanel2.setBackground(new java.awt.Color(255, 0, 0));
+        jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 720, 390));
+
+        jMenuBar1.setBackground(new java.awt.Color(153, 153, 153));
+        jMenuBar1.setBorder(javax.swing.BorderFactory.createTitledBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED), "La Bodeguita", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Century Gothic", 0, 14), new java.awt.Color(255, 51, 51))); // NOI18N
+        jMenuBar1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        jMenu1.setBackground(new java.awt.Color(153, 153, 153));
+        jMenu1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jMenu1.setIcon(new javax.swing.ImageIcon("C:\\Users\\sasuk\\Desktop\\TSW41\\Integradora\\LaBodeguita-master\\LaBodeguita\\src\\imagenes\\inicio 2.png")); // NOI18N
         jMenu1.setContentAreaFilled(false);
+        jMenu1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenu1.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
         jMenu1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -118,12 +141,15 @@ public class Principal extends javax.swing.JFrame {
         });
         jMenuBar1.add(jMenu1);
 
-        jMenu5.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 5));
+        jMenu5.setBackground(new java.awt.Color(153, 153, 153));
+        jMenu5.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jMenu5.setText("Ventas");
         jMenu5.setToolTipText("");
+        jMenu5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenu5.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
 
         jMenuItem4.setText("Venta de Productos");
+        jMenuItem4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem4ActionPerformed(evt);
@@ -133,11 +159,13 @@ public class Principal extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu5);
 
-        MenuControl.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 5));
+        MenuControl.setBackground(new java.awt.Color(153, 153, 153));
+        MenuControl.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         MenuControl.setText("Control");
         MenuControl.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
 
         jMenuItem10.setText("Control y reistro de productos");
+        jMenuItem10.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem10.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem10ActionPerformed(evt);
@@ -146,6 +174,7 @@ public class Principal extends javax.swing.JFrame {
         MenuControl.add(jMenuItem10);
 
         jMenuItem5.setText("Control y registro de ventas");
+        jMenuItem5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem5ActionPerformed(evt);
@@ -154,6 +183,7 @@ public class Principal extends javax.swing.JFrame {
         MenuControl.add(jMenuItem5);
 
         jMenuItem2.setText("Control y registro de trabajadores");
+        jMenuItem2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem2ActionPerformed(evt);
@@ -162,6 +192,7 @@ public class Principal extends javax.swing.JFrame {
         MenuControl.add(jMenuItem2);
 
         jMenuItem6.setText("Control y reabastecimiento de stock");
+        jMenuItem6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem6ActionPerformed(evt);
@@ -170,6 +201,7 @@ public class Principal extends javax.swing.JFrame {
         MenuControl.add(jMenuItem6);
 
         jMenuItem11.setText("Control  y registro de proveedores");
+        jMenuItem11.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem11.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem11ActionPerformed(evt);
@@ -178,6 +210,7 @@ public class Principal extends javax.swing.JFrame {
         MenuControl.add(jMenuItem11);
 
         jMenuItem12.setText("Control  de fiado");
+        jMenuItem12.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem12.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem12ActionPerformed(evt);
@@ -187,9 +220,11 @@ public class Principal extends javax.swing.JFrame {
 
         jMenuBar1.add(MenuControl);
 
-        jMenu6.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 5));
+        jMenu6.setBackground(new java.awt.Color(153, 153, 153));
+        jMenu6.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jMenu6.setText("Stock");
         jMenu6.setToolTipText("Re abastecer stock");
+        jMenu6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenu6.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
         jMenu6.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -203,11 +238,13 @@ public class Principal extends javax.swing.JFrame {
         });
         jMenuBar1.add(jMenu6);
 
-        jMenu2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 5));
+        jMenu2.setBackground(new java.awt.Color(153, 153, 153));
+        jMenu2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jMenu2.setText("Reporte");
         jMenu2.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
 
         jMenuItem3.setText("Reporte de Ventas");
+        jMenuItem3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem3ActionPerformed(evt);
@@ -217,7 +254,8 @@ public class Principal extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu2);
 
-        jMenu4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 5));
+        jMenu4.setBackground(new java.awt.Color(153, 153, 153));
+        jMenu4.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jMenu4.setText("Cerrar sesion");
         jMenu4.setToolTipText("Cerrar sesion del usuario");
         jMenu4.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
@@ -233,9 +271,12 @@ public class Principal extends javax.swing.JFrame {
         });
         jMenuBar1.add(jMenu4);
 
-        jMenu3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 5));
+        jMenu3.setBackground(new java.awt.Color(153, 153, 153));
+        jMenu3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jMenu3.setIcon(new javax.swing.ImageIcon("C:\\Users\\sasuk\\Desktop\\TSW41\\Integradora\\LaBodeguita-master\\LaBodeguita\\src\\imagenes\\salir.png")); // NOI18N
         jMenu3.setText("Salir");
         jMenu3.setToolTipText("Salir de la aplicacion");
+        jMenu3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jMenu3.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
         jMenu3.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -279,8 +320,71 @@ public class Principal extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        
-        // TODO add your handling code here:
+     System.out.println("generando...");
+        String sql="select * from ventas";
+         Workbook book = new HSSFWorkbook();
+         org.apache.poi.ss.usermodel.Sheet sheet =  book.createSheet("Reporte");
+         String date = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+         String file = date+".xls";
+         int rowCount = 1;
+         try{
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            
+            Row rowHead;
+            rowHead = sheet.createRow(0);
+            org.apache.poi.ss.usermodel.Cell headFolio = rowHead.createCell(0);
+            headFolio.setCellValue("Folio");
+            
+            org.apache.poi.ss.usermodel.Cell headDescripcion_de_venta = rowHead.createCell(1);
+            headDescripcion_de_venta.setCellValue("Descripcion de venta");
+            
+            org.apache.poi.ss.usermodel.Cell headVentas_realizadas = rowHead.createCell(2);
+            headVentas_realizadas.setCellValue("Ventas realizadas");
+            
+            org.apache.poi.ss.usermodel.Cell headPrecio_unitario = rowHead.createCell(3);
+            headPrecio_unitario.setCellValue("Precio unitario");
+            
+            org.apache.poi.ss.usermodel.Cell headTotal_de_la_venta = rowHead.createCell(4);
+            headTotal_de_la_venta.setCellValue("Total de la venta");
+            
+            org.apache.poi.ss.usermodel.Cell headFecha = rowHead.createCell(5);
+            headFecha.setCellValue("Fecha");
+
+                
+            while(rs.next()){
+                Row row;
+                row = sheet.createRow(rowCount);
+                
+                org.apache.poi.ss.usermodel.Cell celdaFolio = row.createCell(0);
+                celdaFolio.setCellValue(rs.getString("codVent"));
+                
+                org.apache.poi.ss.usermodel.Cell celdaDescripcion_de_venta = row.createCell(1);
+                celdaDescripcion_de_venta.setCellValue(rs.getString("desVent"));
+                
+                org.apache.poi.ss.usermodel.Cell celdaVentas_realizadas = row.createCell(2);
+                celdaVentas_realizadas.setCellValue(rs.getString("cantVent"));
+                
+                org.apache.poi.ss.usermodel.Cell celdaPrecio_unitario = row.createCell(3);
+                celdaPrecio_unitario.setCellValue(rs.getString("preVent"));
+                
+                org.apache.poi.ss.usermodel.Cell celdaTotal_de_la_venta = row.createCell(4);
+                celdaTotal_de_la_venta.setCellValue(rs.getString("totalVent"));
+                
+                org.apache.poi.ss.usermodel.Cell celdaFecha = row.createCell(5);
+                celdaFecha.setCellValue(rs.getString("feVent"));
+                
+                
+                
+                rowCount++;
+            }
+            FileOutputStream out = new FileOutputStream(file);
+            book.write(out);
+            JOptionPane.showMessageDialog(null, "Reporte creado");
+            out.close();
+         } catch (Exception e) {
+             
+         }        
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenu3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenu3MouseClicked
@@ -336,6 +440,10 @@ public class Principal extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jMenuItem12ActionPerformed
 
+    private void valorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_valorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_valorActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -376,6 +484,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JLabel fecha;
     private javax.swing.JLabel hora;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
@@ -391,6 +500,8 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     public static javax.swing.JLabel trabajador;
     public static javax.swing.JTextField valor;
     // End of variables declaration//GEN-END:variables
